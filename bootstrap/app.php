0b86b2caaa55ed8d1cc074036d29183493248d9e
m4ry4md <m4ry4md@gmail.com>
2025-07-17 20:24:28 +0330

    feat: Add auth routes and move authentication logic
    Description:
    
    feat: Add auth route file to handle authentication-related routes.
    
    refactor: Move all authentication routes to the new auth.php file for better organization.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +13,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         channels: __DIR__.'/../routes/channels.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->prefix('api/auth')
+                ->group(base_path('routes/auth.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
---
+++ routes/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Auth routes
+
+Route::prefix('v1')->as('api.v1.auth.')
+
+    ->group(function () {
+        Route::middleware('apiGuest')->group(function () {
+            Route::post('/register', [\App\Http\Controllers\Api\V1\Auth\RegisterController::class, 'register'])
+                ->middleware(['throttle:register'])
+                ->name('register');
+
+            Route::post('/login', [\App\Http\Controllers\Api\V1\Auth\LoginController::class, 'login'])
+                ->middleware(['throttle:login','throttle:login_with_same_email'])
+                ->name('login');
+        });
+
+        Route::post('/logout', \App\Http\Controllers\Api\V1\Auth\LogoutController::class)
+            ->middleware('auth:sanctum')
+            ->name('logout');
+
+    });
+
+
+
